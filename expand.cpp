0ec3611efe3a44a496a8db0282f9f2d9a8155b46
LeoggYY <153478805+LeoggYY@users.noreply.github.com>
2024-10-30 18:52:16 +0800

    Update expand.cpp

--- expand.cpp
+++ expand.cpp
@@ -13,7 +13,7 @@ void change_size(int*& arr, int size, int new_size) {
     copy(arr, arr + size, new_arr);
 
     // Increase every elements by size.
-    for (int i = 0; i < new_size; i++) new_arr[i] *= new_size;
+    for (int i = 0; i < new_size; i++) new_arr[i] += new_size;
 
     delete[] recycler;
     arr = new_arr;
@@ -24,13 +24,13 @@ int main() {
 
     // Inserting elements.
     for (int i = 0; i < size; i++) {
-        arr[i] = i + 1;
+        arr[i] = i\+1;
     };
     print_arr(arr, size);  // Print initial array.
-
+	
     int new_size;
-    cin >> new_size;
-
+    cin >> new_size;	
+	
     change_size(arr, size, new_size);
     print_arr(arr, new_size);  // Size has changed, print the new array.
 }
